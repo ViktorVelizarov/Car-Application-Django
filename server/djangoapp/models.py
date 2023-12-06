f4b31df8c42dd3307dc8687eeb0fdc81885b6127
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-12-06 08:51:00 +0100

    update models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,44 +1,22 @@
 from django.db import models
 from django.utils.timezone import now
 
-# Create your models here.
-
+# <HINT> Create a Car Make model `class CarMake(models.Model)`:
 class CarMake(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    # Add any other fields you want for CarMake model
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
 
     def __str__(self):
         return self.name
 
-
+# <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    dealer_id = models.CharField(max_length=50)  # Assuming dealer_id is a string, adjust as needed
-    CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        # Add more choices as needed
-    ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES)
+    name = models.CharField(max_length=30)
+    type_c = models.CharField(max_length=10, choices=(('Sedan', 'Sedan',), ('SUV', 'SUV'), ('HATCHBACK', 'HATCHBACK'),('WAGON', 'WAGON')))
+    dealer_id = models.IntegerField()
     year = models.DateField()
-    # Add any other fields you want for CarModel model
 
     def __str__(self):
-        return f"{self.car_make.name} - {self.name}"
+        return self.name
 
-
-class CarDealer:
-    def __init__(self, dealer_id, name, location):
-        self.dealer_id = dealer_id
-        self.name = name
-        self.location = location
-
-
-class DealerReview:
-    def __init__(self, dealer_id, review_text, rating):
-        self.dealer_id = dealer_id
-        self.review_text = review_text
-        self.rating = rating
